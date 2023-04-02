39090702480a2f490c26607419b135bdaefade62
Quoc Mien <quocmienn@gmail.com>
2023-04-02 20:46:39 +0700

    update

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def generate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=5000,use_reloader=True)
+    app.run(debug=True,host="0.0.0.0", port=7000,use_reloader=True)
